559cf26017395ea6dd7a929a9f53947a73259eb1
sri-git-acc <sriy0697@gmail.com>
2025-02-17 18:59:33 +0530

    added egress to the script

--- main.tf
+++ main.tf
@@ -2,6 +2,13 @@ resource "aws_security_group" "main" {
   name        = local.sg_final_name
   vpc_id      = var.vpc_id
 
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   tags = merge(
     var.common_tags,
     var.sg_tags,
